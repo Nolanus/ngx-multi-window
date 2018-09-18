9ecdb889024575a6c219ce329ff46d0712c9b554
Unknown <kayvanbree@gmail.com>
2018-09-18 12:40:50 +0200

    Copy paste everything into new Angular 6 project

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { MultiWindowModule } from 'ngx-multi-window';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MultiWindowModule
+  ],
+  providers: [Location, {provide: LocationStrategy, useClass: PathLocationStrategy}],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}

71f848ec09982c5418936141d61b26cf6cc84017
Unknown <kayvanbree@gmail.com>
2018-10-03 17:39:19 +0200

    Test on headless chromium

--- projects/ngx-multi-window/karma.conf.js
+++ projects/ngx-multi-window/karma.conf.js
@@ -25,7 +25,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chromium_no_sandbox'],
+    customLaunchers: {
+      Chromium_no_sandbox: {
+        base: 'ChromiumHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false
   });
 };

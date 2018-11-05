0b64f449bbd2cfbd56965a829f5a6a748a85ee55
Kay van Bree <kayvanbree@gmail.com>
2018-11-05 13:55:29 +0100

    Add config.provider to index.ts exports

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,28 +1,31 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed, async} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {NGXMW_CONFIG} from 'ngx-multi-window';
+import {FormsModule} from '@angular/forms';
+
+const multiWindowConfig = {
+  heartbeat: 1000,
+  newWindowScan: 1000
+};
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent,
+        AppComponent
       ],
+      providers: [
+        {provide: NGXMW_CONFIG, useValue: multiWindowConfig}
+      ],
+      imports: [
+        FormsModule
+      ]
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+  });
 });

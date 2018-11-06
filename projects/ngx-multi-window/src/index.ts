cb78fee63ded35171b0c76d6859898cdd460098f
Kay van Bree <kayvanbree@gmail.com>
2018-11-06 13:31:00 +0100

    Add config.provider to index.ts exports (#40)
    * Add config.provider to index.ts exports
    
    * #39 Add trailing comma's

--- projects/ngx-multi-window/src/index.ts
+++ projects/ngx-multi-window/src/index.ts
@@ -6,6 +6,7 @@ export * from './lib/multi-window.module';
 
 export * from './lib/providers/multi-window.service';
 export * from './lib/providers/storage.service';
+export * from './lib/providers/config.provider';
 
 export * from './lib/types/message.type';
 export * from './lib/types/multi-window.config';

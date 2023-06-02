2476b0f81ee439b9a28653885a134d5885b081f7
Jared <TheWolfBadger@gmail.com>
2023-06-01 21:53:25 -0400

    Start new project off of this old code and make use of BroadcastChannel API rather than using storage for passing messages...

--- projects/ngx-multi-window/src/lib/types/window.type.ts
+++ projects/ngx-multi-window/src/lib/types/window.type.ts
@@ -1,24 +1,3 @@
-import { Message } from './message.type';
-
-export interface WindowData {
-  heartbeat: number;
+export interface KnownAppWindow {
   id: string;
-  name: string;
-}
-
-export interface KnownAppWindow extends WindowData {
-  stalled: boolean;
-  self: boolean;
-}
-
-/**
- * Represents a window with the current application how it is stored
- * in the localstorage. It has an id, name, heartbeat represented in a timestamp
- * and an array of messages that that window sent out.
- *
- * It's named "AppWindow" to avoid confusion with the type Window
- * of the global variable "window".
- */
-export interface AppWindow extends WindowData {
-  messages: Message[];
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,15 +12,9 @@ export class AppComponent implements OnInit {
   ownName: string;
   ownId: string;
 
-  windows: KnownAppWindow[] = [];
   logs: string[] = [];
 
   newName: string;
-
-  @HostListener('window:unload')
-  unloadHandler() {
-    this.multiWindowService.saveWindow();
-  }
 
   constructor(private multiWindowService: MultiWindowService, private nameGenerator: NameGeneratorService) {
   }
@@ -32,72 +26,24 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.ownId = this.multiWindowService.id;
-    this.ownName = this.multiWindowService.name;
-    if (this.ownName.indexOf(this.ownId) >= 0) {
-      // This window still has the automatic given name, so generate a fake one for demo reasons
-      // Generate a random name for the current window, just for fun
-      this.multiWindowService.name = this.ownName = this.nameGenerator.getRandomFakeName();
-    }
     this.newName = this.ownName;
-    this.windows = this.multiWindowService.getKnownWindows();
-    if (this.multiWindowService.getKnownWindows().length > 0) {
-      this.multiWindowService.onMessage().subscribe((value: Message) => {
-        if (value.senderId != this.ownId) {
-          this.logs.unshift('Received a message from ' + value.senderId + ': ' + value.data);
-        }
-      });
-    }
-
-    this.multiWindowService.onWindows().subscribe(knownWindows => this.windows = knownWindows);
+    this.multiWindowService.onMessage('data').subscribe((value: Message) => {
+      if (value.senderId != this.ownId) {
+        this.logs.unshift('Received a message from ' + value.senderId + ': ' + value.data);
+      }
+    });
   }
 
-  public sendTonsOfMessages(recipientId: string, message: string) {
-    for (let i = 0; i < 5000; i++) {
-      this.sendMessage(recipientId, message);
-    }
-  }
-
-  public sendMessage(recipientId: string, message: string) {
-    if (recipientId === this.ownId) {
-      // Catch sending messages to itself. Trying to do so throws an error from multiWindowService.sendMessage()
-      this.logs.unshift('Can\'t send messages to itself. Select another window.');
-
-      return;
-    }
-    this.multiWindowService.sendMessage(recipientId, 'customEvent', message).subscribe(
-      (messageId: string) => {
-        this.logs.unshift('Message send, ID is ' + messageId);
-      },
-      (error) => {
-        this.logs.unshift('Message sending failed, error: ' + error);
-      },
-      () => {
-        this.logs.unshift('Message successfully delivered');
-      });
+  public sendMessage(message: string) {
+    this.multiWindowService.sendMessage("data", message);
   }
 
   public removeLogMessage(index: number) {
     this.logs.splice(index, 1);
   }
 
-  public changeName() {
-    this.multiWindowService.name = this.ownName = this.newName;
-  }
-
   public newWindow() {
-    const newWindowData = this.multiWindowService.newWindow();
-    newWindowData.created.subscribe({
-        next: () => {
-        },
-        error: (err) => {
-          this.logs.unshift('An error occured while waiting for the new window to start consuming messages');
-        },
-        complete: () => {
-          this.logs.unshift('The new window with id ' + newWindowData.windowId + ' got created and starts consuming messages');
-        }
-      }
-    );
-    window.open('?' + newWindowData.urlString);
+    window.open('?');
   }
 
   public windowTrackerFunc(item, index) {

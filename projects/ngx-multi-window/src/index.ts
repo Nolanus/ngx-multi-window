b596741e8f4a2d098ecacc5667885eb89d21b192
Sebastian Fuss <sebastian.fuss@googlemail.com>
2023-10-03 14:04:21 +0200

    refactor: remove native window provider

--- projects/ngx-multi-window/src/index.ts
+++ projects/ngx-multi-window/src/index.ts
@@ -5,7 +5,6 @@
 export * from './lib/multi-window.module';
 
 export * from './lib/providers/multi-window.service';
-export * from './lib/providers/window.provider';
 
 export * from './lib/types/message.type';
 export * from './lib/types/window.type';
--- projects/ngx-multi-window/src/lib/multi-window.module.ts
+++ projects/ngx-multi-window/src/lib/multi-window.module.ts
@@ -2,13 +2,11 @@ import { CommonModule } from '@angular/common';
 import { ModuleWithProviders, NgModule } from '@angular/core';
 
 import { MultiWindowService } from './providers/multi-window.service';
-import { WindowRef } from './providers/window.provider';
 
 @NgModule({
   imports: [CommonModule],
   providers: [
     MultiWindowService,
-    WindowRef
   ],
 })
 export class MultiWindowModule {}
